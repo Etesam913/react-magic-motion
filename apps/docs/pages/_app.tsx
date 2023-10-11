73f1c4967a83e2fa88b429b4bcd52c67ebb21e5b
etesam <etesamansari13@gmail.com>
2023-10-10 23:20:47 -0400

    :lipstick: Added `Space Mono` for code font-family
    Added a 404 error page

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,5 +1,22 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import { Space_Mono } from "next/font/google";
+
+const spaceMono = Space_Mono({
+  style: "normal",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        code {
+          font-family: ${spaceMono.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
