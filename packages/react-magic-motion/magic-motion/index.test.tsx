b74cab220b341118e96438cea2aec8270452e496
etesam <etesamansari13@gmail.com>
2023-11-04 15:44:40 -0400

    :white_check_mark: Added test to check `key="exclude"` property

--- packages/react-magic-motion/magic-motion/index.test.tsx
+++ packages/react-magic-motion/magic-motion/index.test.tsx
@@ -23,7 +23,7 @@ describe("<MagicMotion> tests", () => {
       })),
     });
   });
-  test("Simple example with two children", () => {
+  test("div with two div children", () => {
     const children = (
       <div data-testid="parent-container">
         <div data-testid="child-1">child1</div>
@@ -46,12 +46,56 @@ describe("<MagicMotion> tests", () => {
     const child1 = parentNode.props.children.at(0);
     const child2 = parentNode.props.children.at(1);
 
+    // Checks if child 1 is a motion component
     expect(child1.type.render.name === "MotionComponent").toBeTruthy();
     expect(child1.props.layout === "position").toBeTruthy();
     expect(child1.props.children[0] === "child1").toBeTruthy();
+
+    // Checks if child 2 is a motion component
     expect(child2.type.render.name === "MotionComponent").toBeTruthy();
     expect(child2.props.layout === "position").toBeTruthy();
     expect(child2.props.children[0] === "child2").toBeTruthy();
+
+    const { getByTestId } = render(<MagicMotion>{children}</MagicMotion>);
+    expect(getByTestId("parent-container")).toBeInTheDocument();
+    expect(getByTestId("child-1")).toBeInTheDocument();
+    expect(getByTestId("child-2")).toBeInTheDocument();
+  });
+
+  test("div with two 1 div child and 1 div child with key='exclude'", () => {
+    const children = (
+      <div data-testid="parent-container">
+        <div data-testid="child-1">child1</div>
+        <div data-testid="child-2" key="exclude">
+          child2
+        </div>
+      </div>
+    );
+    const motionChildren = convertChildrenToMotionChildren(
+      children,
+      (child) => {
+        return {
+          layout: getLayoutValueFromChildren(child),
+        };
+      }
+    ) as any[];
+
+    expect(motionChildren).toBeDefined();
+    const parentNode = motionChildren.at(0);
+    expect(parentNode.type.render.name === "MotionComponent").toBeTruthy();
+    expect(parentNode.props.layout === true).toBeTruthy();
+    const child1 = parentNode.props.children.at(0);
+    const child2 = parentNode.props.children.at(1);
+
+    // Check if child 1 is a motion component
+    expect(child1.type.render.name === "MotionComponent").toBeTruthy();
+    expect(child1.props.layout === "position").toBeTruthy();
+    expect(child1.props.children[0] === "child1").toBeTruthy();
+
+    // Check if child 2 is not a motion component as it is excluded
+    expect(child2.type === "div").toBeTruthy();
+    expect(child2.props.layout === undefined).toBeTruthy();
+    expect(child2.props.children === "child2").toBeTruthy();
 
     const { getByTestId } = render(<MagicMotion>{children}</MagicMotion>);
     expect(getByTestId("parent-container")).toBeInTheDocument();
